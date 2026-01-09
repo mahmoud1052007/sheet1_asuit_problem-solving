22d12db56f42f8113d4b965c1ea8e06a24ba0eb3
Mahmoud_Ashraf <mmaahmoudashraff@gmail.com>
2026-01-09 18:07:17 +0200

    (B) Basic Data Types.

---
+++ (B) Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    long long l;
+    char ch;
+    float fl;
+    double dou;
+    cin>>i>>l>>ch>>fl>>dou;
+    cout<<i<<endl;
+    cout<<l<<endl;
+    cout<<ch<<endl;
+    cout<<fl<<endl;
+    cout<<dou<<endl;
+    return 0;
+}
